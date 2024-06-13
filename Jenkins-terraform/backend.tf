a27cc28b35e4ed9d24b537be896c32fa80b6ea14
Aduraleke Faith Akintade <103920213+leketech@users.noreply.github.com>
2024-06-13 15:19:07 +0100

    Update backend.tf

--- Jenkins-terraform/backend.tf
+++ Jenkins-terraform/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "leke-3" # Replace with your actual S3 bucket name
-    key    = "Jenkins-terraform/terraform.tfstate"
+    key    = "Jenkins/terraform.tfstate"
     region = "us-east-1"
   }
 }
